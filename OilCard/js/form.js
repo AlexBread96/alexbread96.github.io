322352f45b9b9d2ef3314ba067e39ba359d1bfa0
AlexBread96 <63932653+AlexBread96@users.noreply.github.com>
2020-06-09 23:35:24 +0300

    Add files via upload

---
+++ OilCard/js/form.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$("form").submit(function () { // Событие отправки с формы
+		var form_data = $(this).serialize(); // Собираем данные из полей
+		$.ajax({
+			type: "POST", // Метод отправки
+			url: "sendform.php", // Путь к PHP обработчику sendform.php
+			data: form_data,
+			success: function () {
+				$('.pop__thx').addClass('active');
+			}
+		});
+		event.preventDefault();
+	});
+});
